2b3eed30447c3b4213417d2497de140c10dca6ac
Andrey <C:\Users\Andrey\AppData\Roaming\The Bat!>
2022-08-20 22:22:12 +0300

    Добавил решение

---
+++ task66/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int M = 0;
+int N = 0;
+int sum = 0;
+
+int.TryParse(Console.ReadLine(), out M);
+int.TryParse(Console.ReadLine(), out N);
+
+string Function(int M, int N)
+{
+    sum += M;
+    if (M == N) return sum.ToString();
+    else return (Function(M + 1, N));
+}
+if (M < N) Console.Write($"Сумма элементов: {Function(M, N)}");
+else Console.WriteLine("Нкверный ввод! ");
---
+++ task68/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+// m = 2, n = 3 -> A(m,n) = 29
+
+Console.Write("Введите n: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите m: ");
+int m = int.Parse(Console.ReadLine()!);
+
+int Ankerman(int n, int m)
+{
+    if (n == 0) return m + 1;
+    if (n != 0 && m == 0) return Ankerman(n - 1, 1);
+    if (n > 0 && m > 0) return Ankerman(n - 1, Ankerman(n, m - 1));
+    return Ankerman(n, m);
+}
+Console.WriteLine(Ankerman(n, m));
